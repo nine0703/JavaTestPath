bf66ab755e5e5c54062d6c65ece29e2a02063ce7
TK.ENDO <2283148902@qq.com>
2024-03-08 23:50:25 +0800

    Update Fri Mar 08 ⏱ 23:45 <CST> 2024

--- src/UpdateTimeMachine.java
+++ src/UpdateTimeMachine.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -9,6 +12,19 @@ public class UpdateTimeMachine {
         s += sdf.format(time);
         System.out.println(s);
 
+
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+        // 创建字符串选择对象
+        StringSelection stringSelection = new StringSelection(s);
+
+        // 将字符串选择对象设置到剪贴板中
+        try {
+            clipboard.setContents(stringSelection, null);
+            System.out.println("字符串已成功复制到剪贴板！");
+        } catch (Exception e) {
+            System.err.println("无法复制字符串到剪贴板：" + e.getMessage());
+        }
 
     }   //  method main end.
 
